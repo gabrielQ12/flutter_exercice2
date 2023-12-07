0c256f40b51a727aa89442535e0228ab1707aacd
gabrielq12 <gabriel.quinette1@gmail.com>
2023-12-07 17:09:56 +0100

    TextField: DONE

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -7,15 +7,27 @@ class ProfilePage extends StatefulWidget {
 }
 
 class ProfilePageState extends State<ProfilePage> {
-  Profile myProfile = Profile(surname: "Gabriel", name: "gabrielQ12");
+  Profile myProfile = Profile(surname: "Gabriel", name: "Quinette");
+  late TextEditingController surname;
+  late TextEditingController name;
+  late TextEditingController secret;
 
   @override
   void initState() {
     super.initState();
+    surname = TextEditingController();
+    name = TextEditingController();
+    secret = TextEditingController();
+    surname.text = myProfile.surname;
+    name.text = myProfile.name;
+    secret.text = myProfile.secret;
   }
 
   @override
   void dispose() {
+    surname.dispose();
+    name.dispose();
+    secret.dispose();
     super.dispose();
   }
 
@@ -28,29 +40,69 @@ class ProfilePageState extends State<ProfilePage> {
         mainAxisSize: MainAxisSize.max,
         children: [
           Card(
-            color: Colors.deepPurpleAccent.shade100,
+              color: Colors.deepPurpleAccent.shade100,
               elevation: 10,
               child: Container(
-            width: MediaQuery.of(context).size.width,
-
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(8),
-            child: Column(
-              children: [
-                Text(myProfile.setName()),
-                Text("Age: ${myProfile.setAge()}"),
-                Text("Taille: ${myProfile.setHeight()} "),
-                Text("Genre: ${myProfile.genderString()}"),
-                Text("Hobbies: ${myProfile.setHobbies()}"),
-                Text("Langage de programmation favori: ${myProfile.favoritelang}"),
-              ],
-            ),
-          )
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Text(myProfile.setName()),
+                    Text("Age: ${myProfile.setAge()}"),
+                    Text("Taille: ${myProfile.setHeight()} "),
+                    Text("Genre: ${myProfile.genderString()}"),
+                    Text("Hobbies: ${myProfile.setHobbies()}"),
+                    Text(
+                        "Langage de programmation favori: ${myProfile.favoritelang}"),
+                  ],
+                ),
+              )),
+          const Divider(
+            color: Colors.deepPurpleAccent,
+            thickness: 2,
           ),
-          Divider(color: Colors.deepPurpleAccent, thickness: 2,)
+          const Text("Modifier les informations",
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              )),
+          myTextField(controller: surname, hint: "Entrez votre prénom"),
+          myTextField(controller: name, hint: "Entrez votre nom"),
+          myTextField(controller: secret, hint: "Dites nous un secret", isSecret: true,)
         ],
-      )
-      ),
+      )),
     );
+  }
+
+  TextField myTextField(
+      {required TextEditingController controller,
+      required String hint,
+      bool isSecret = false}) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hint,
+      ),
+      obscureText: isSecret,
+      onSubmitted: ((newValue) {
+        updateUser();
+      }),
+    );
+  }
+
+   updateUser() {
+    setState(() {
+      myProfile = Profile(
+        surname: (surname.text != myProfile.surname)
+            ? surname.text
+            : myProfile.surname,
+        name: (name.text != myProfile.name)
+          ? name.text
+          : myProfile.name,
+        secret: secret.text,
+      );
+    });
   }
 }
