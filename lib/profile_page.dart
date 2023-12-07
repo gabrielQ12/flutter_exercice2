21b5cce20abdf791de8dd9565ff46348cc4999b4
gabrielq12 <gabriel.quinette1@gmail.com>
2023-12-07 18:49:33 +0100

    ajout radios

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -48,94 +48,87 @@ class ProfilePageState extends State<ProfilePage> {
       appBar: AppBar(title: Text("Mon profile")),
       body: SingleChildScrollView(
           child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Card(
-              color: Colors.deepPurpleAccent.shade100,
-              elevation: 10,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  children: [
-                    Text(myProfile.setName()),
-                    Text("Age: ${myProfile.setAge()}"),
-                    Text("Taille: ${myProfile.setHeight()} "),
-                    Text("Genre: ${myProfile.genderString()}"),
-                    Text("Hobbies: ${myProfile.setHobbies()}"),
-                    Text(
-                        "Langage de programmation favori: ${myProfile.favoritelang}"),
-                    ElevatedButton(
-                        onPressed: updateSecret,
-                        child: Text((showSecret)
-                            ? "Cache le secret"
-                            : "Montre le secret")),
-                    (showSecret)
-                        ? Text(myProfile.secret)
-                        : Container(height: 0, width: 0),
-                  ],
-                ),
-              )),
-          const Divider(
-            color: Colors.deepPurpleAccent,
-            thickness: 2,
-          ),
-          const Text("Modifier les informations",
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              )),
-          myTextField(controller: surname, hint: "Entrez votre prénom"),
-          myTextField(controller: name, hint: "Entrez votre nom"),
-          myTextField(
-              controller: secret, hint: "Dites nous un secret", isSecret: true),
-          Row(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Genre:${myProfile.genderString()}"),
-              Switch(
-                  value: myProfile.gender,
-                  onChanged: ((newBool) {
-                    setState(() {
-                      myProfile.gender = newBool;
-                    });
-                  })),
-
+              Card(
+                  color: Colors.deepPurpleAccent.shade100,
+                  elevation: 10,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Text(myProfile.setName()),
+                        Text("Age: ${myProfile.setAge()}"),
+                        Text("Taille: ${myProfile.setHeight()} "),
+                        Text("Genre: ${myProfile.genderString()}"),
+                        Text("Hobbies: ${myProfile.setHobbies()}"),
+                        Text(
+                            "Langage de programmation favori: ${myProfile.favoritelang}"),
+                        ElevatedButton(
+                            onPressed: updateSecret,
+                            child: Text((showSecret)
+                                ? "Cache le secret"
+                                : "Montre le secret")),
+                        (showSecret)
+                            ? Text(myProfile.secret)
+                            : Container(height: 0, width: 0),
+                      ],
+                    ),
+                  )),
+              const Divider(
+                color: Colors.deepPurpleAccent,
+                thickness: 2,
+              ),
+              myTitle("Modifier les infos"),
+              myTextField(controller: surname, hint: "Entrez votre prénom"),
+              myTextField(controller: name, hint: "Entrez votre nom"),
+              myTextField(
+                  controller: secret, hint: "Dites nous un secret", isSecret: true),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Genre:${myProfile.genderString()}"),
+                  Switch(
+                      value: myProfile.gender,
+                      onChanged: ((newBool) {
+                        setState(() {
+                          myProfile.gender = newBool;
+                        });
+                      })),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Taille:${myProfile.setHeight()} "),
+                  Slider(
+                      value: myProfile.height,
+                      min: 0,
+                      max: 250,
+                      onChanged: ((newHeight) {
+                        setState(() {
+                          myProfile.height = newHeight;
+                        });
+                      }))
+                ],
+              ),
+              const Divider(color: Colors.deepPurpleAccent, thickness: 2),
+              myHobbies(),
+              const Divider(color: Colors.deepPurpleAccent, thickness: 2),
+              myRadios(),
             ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("Taille:${myProfile.setHeight()} "),
-              Slider(
-                value: myProfile.height,
-                min: 0,
-                max: 250,
-                onChanged: ((newHeight) {
-                  setState((){
-                    myProfile.height = newHeight;
-                  });
-                })
-              )
-            ],
-          ),
-          Divider(color: Colors.deepPurpleAccent, thickness: 2),
-          myHobbies(),
-          Divider(color: Colors.deepPurpleAccent, thickness: 2),
-
-        ],
-      )),
+          )),
     );
   }
 
   TextField myTextField(
       {required TextEditingController controller,
-      required String hint,
-      bool isSecret = false}) {
+        required String hint,
+        bool isSecret = false}) {
     return TextField(
       controller: controller,
       decoration: InputDecoration(
@@ -167,25 +160,27 @@ class ProfilePageState extends State<ProfilePage> {
   }
 
   Column myHobbies() {
-    List<Widget> widgets = [];
+    List<Widget> widgets = [myTitle("Mes Hobbies")];
     hobbies.forEach((hobby, like) {
       Row r = Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.max,
         children: [
           Text(hobby),
-          Checkbox(value: like, onChanged: (newBool) {
-            setState(() {
-              hobbies[hobby] = newBool ?? false;
-              List<String> str = [];
-              hobbies.forEach((key, value) {
-                if(value == true) {
-                  str.add(key);
-                }
-              });
-              myProfile.hobbies = str;
-            });
-          })
+          Checkbox(
+              value: like,
+              onChanged: (newBool) {
+                setState(() {
+                  hobbies[hobby] = newBool ?? false;
+                  List<String> str = [];
+                  hobbies.forEach((key, value) {
+                    if (value == true) {
+                      str.add(key);
+                    }
+                  });
+                  myProfile.hobbies = str;
+                });
+              })
         ],
       );
       widgets.add(r);
@@ -193,5 +188,53 @@ class ProfilePageState extends State<ProfilePage> {
     return Column(children: widgets);
   }
 
+  Column myRadios() {
+    List<Widget> w = [];
+    List<String> langs = [
+      "Dart",
+      "Swift",
+      "Kotlin",
+      "JavaScript",
+      "Python",
+      "SQL"
+    ];
+    int index =
+    langs.indexWhere((lang) => lang.startsWith(myProfile.favoritelang));
+    for (var x = 0; x < langs.length; x++) {
+      Column c = Column(
 
+        children: [
+          Text(langs[x]),
+          Radio(
+            value: x,
+            groupValue: index,
+            onChanged: ((newValue) {
+              setState(() {
+                myProfile.favoritelang = langs[newValue as int];
+              });
+            }),
+          )
+        ],
+      );
+      w.add(c);
+    }
+    return Column(
+      children: [
+        myTitle("Language préféré"),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: w
+        )
+      ],
+    );
+  }
+
+  Text myTitle(String text) {
+    return Text(text,
+        style: const TextStyle(
+          color: Colors.deepPurple,
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ));
+  }
 }
